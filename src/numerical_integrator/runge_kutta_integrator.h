cbe4629778322404f27f29c805bc53771392bc4d
ChanghyeonKim93 <hyun91015@gmail.com>
2023-10-18 01:50:44 +0900

    d

--- src/numerical_integrator/runge_kutta_integrator.h
+++ src/numerical_integrator/runge_kutta_integrator.h
@@ -1,6 +1,7 @@
 #ifndef NUMERICAL_INTEGRATOR_RUNGE_KUTTA_INTEGRATOR_H
 #define NUMERICAL_INTEGRATOR_RUNGE_KUTTA_INTEGRATOR_H
 
+#include <memory>
 #include <vector>
 
 #include "eigen3/Eigen/Dense"
@@ -9,115 +10,45 @@
 StateDerivativeFunction
 - NonlinearStateDerivativeFunction
 - LinearStateDerivativeFunction
+*/
 
-state_derivative_functor = new
-NonlinearStateDerivativeFunction<TwoWheelRobotKinematics, 3, 2>(new
-TwoWheelRobotKinematics());
+/*
+numerical_integrator::RungeKuttaFourthOrderIntegrator
+  runge_kutta_fourth_order_integrator(new StateDerivativeFunction<3, 2>(new
+  TwoWheeledKinematicsFunctor()));
 
-  return (new ceres::AutoDiffCostFunction<SnavelyReprojectionError, 2, 9, 3>(
-      new SnavelyReprojectionError(observed_x, observed_y)));
+
+  runge_kutta_fourth_order_integrator.Integrate(dt, X_k, u_k);
 
 */
 
 namespace numerical_integrator {
 
-// template <int kDimensionOfStateVector,  // dimension of state vector
-//           int kDimensionOfInputVector>  // dimension of input vector
-// class StateDerivativeFunction {
-//  public:
-//   StateDerivativeFunction()
-//       : dimension_of_state_vector_(kDimensionOfStateVector),
-//         dimension_of_input_vector_(kDimensionOfInputVector) {}
-
-//   virtual ~StateDerivativeFunction() {}
-
-//   virtual bool EvaluateStateDerivative(
-//       const Eigen::Matrix<kDimensionOfStateVector, 1, double>& X_k,
-//       const Eigen::Matrix<kDimensionOfInputVector, 1, double>& u_k);
-
-//  protected:
-//   const int GetDimensionOfStateVector() { return dimension_of_state_vector_;
-//   } const int GetDimensionOfInputVector() { return
-//   dimension_of_input_vector_; }
-
-//  private:
-//   int dimension_of_state_vector_;
-//   int dimension_of_input_vector_;
-// };
-
-template <int kDimensionOfStateVector,  // Dimension of state vector
-          int kDimensionOfInputVector>  // Dimension of input vector
-class NonlinearDerivativeFunction {
+template <typename StateDerivativeFunctor,
+          int kDimensionOfStateVector,  // dimension of state vector
+          int kDimensionOfInputVector>  // dimension of input vector
+class SizedStateDerivativeFunction {
  public:
-  explicit NonlinearDerivativeFunction()
-      : dimension_of_state_vector_(kDimensionOfStateVector),
-        dimension_of_input_vector_(kDimensionOfInputVector) {}
-  ~NonlinearDerivativeFunction() {}
+  explicit SizedStateDerivativeFunction(
+      const StateDerivativeFunctor* state_derivative_functor)
+      : state_derivative_functor_(state_derivative_functor) {}
 
-  virtual bool EvaluateStateDerivative(
-      const Eigen::Matrix<kDimensionOfStateVector, 1, double>& X_k,
-      const Eigen::Matrix<kDimensionOfInputVector, 1, double>& u_k) = 0;
+  ~SizedStateDerivativeFunction() {}
 
- protected:
-  const int GetDimensionOfStateVector() { return dimension_of_state_vector_; }
-  const int GetDimensionOfInputVector() { return dimension_of_input_vector_; }
+  bool EvaluateStateDerivative(
+      const Eigen::Matrix<double, kDimensionOfStateVector, 1>& X_k,
+      const Eigen::Matrix<double, kDimensionOfInputVector, 1>& u_k,
+      Eigen::Matrix<double, kDimensionOfStateVector, 1>*
+          state_derivative_vector) {
+    state_derivative_functor_->EvaluateStateDerivative(
+        X_k.data(), u_k.data(), state_derivative_vector->data());
 
- private:
-  int dimension_of_state_vector_;
-  int dimension_of_input_vector_;
-};
-
-class TwoWheeledRobotKinematics
-    : public numerical_integrator::NonlinearDerivativeFunction<3, 2> {
- public:
-  TwoWheeledRobotKinematics() { GetDimensionOfInputVector(); }
-
-  bool EvaluateStateDerivative(const Eigen::Matrix<3, 1, double>& X_k,
-                               const Eigen::Matrix<2, 1, double>& u_k) {
+    std::cout << state_derivative_vector->transpose() << std::endl;
     return true;
   }
-};
 
-RungeKutta integrator(new TwoWheeledRobotKinematics());
-integrator.IntegrateStateVector(dt, X_k, u_k);
-const auto X_k1 = integrator.GetPreviouslyIntegratedStateVector();
-
-// template <typename DerivativeFunctor,
-//           int kDimensionOfStateVector,  // Dimension of state vector
-//           int kDimensionOfInputVector>  // Dimension of input vector
-// class LinearDerivativeFunctor
-//     : public SizedDerivativeFunction<kDimensionOfStateVector,
-//                                      kDimensionOfInputVector> {
-//  public:
-//   ~LinearDerivativeFunctor() {}
-
-//   virtual bool Evaluate(
-//       const Eigen::Matrix<kDimensionOfStateVector, 1, double>& X_k,
-//       const Eigen::Matrix<kDimensionOfInputVector, 1, double>& u_k) = 0;
-
-//  private:
-//   Eigen::Matrix<kDimensionOfStateVector, kDimensionOfStateVector, double>
-//       system_matrix_;  // A
-//   Eigen::Matrix<kDimensionOfStateVector, kDimensionOfInputVector, double>
-//       input_matrix_;  // B
-// };
-
-class MyDerivativeFunctor : public NonlinearDerivativeFunction<3, 2> {};
-
-template <int state_dimension, int input_dimension>
-class RungeKuttaIntegrator {
- public:
-  virtual ~RungeKuttaIntegrator() = 0;
-
- public:
-  virtual void Reset() = 0;
-  virtual double Integrate(const Eigen::Matrix<state_dimension, 1, double>& X_k,
-                           const Eigen::Matrix<input_dimension, 1, double>& u_k,
-                           const NonlinearDerivativeFunctor& dX_k,
-                           const double dt) = 0;
-
- protected:
-  std::vector<Eigen::Matrix<state_dimension, 1, double>> estimated_state_list_;
+ private:
+  const StateDerivativeFunctor* state_derivative_functor_;
 };
 
 }  // namespace numerical_integrator
--- test/test_runge_kutta.cc
+++ test/test_runge_kutta.cc
@@ -1,10 +1,85 @@
 #include <iostream>
 
 #include "src/numerical_integrator/runge_kutta_integrator.h"
-#include "src/numerical_integrator/runge_kutta_integrator_fourth_order.h"
+
+using namespace numerical_integrator;
+
+class Kinematics {
+ public:
+  virtual bool EvaluateStateDerivative(
+      const double* const current_state_vector,
+      const double* const current_input_vector,
+      double* current_state_derivative_vector) const = 0;
+};
+
+class TwoWheeledRobotKinematics : public Kinematics {
+ public:
+  explicit TwoWheeledRobotKinematics(
+      const double length_between_wheel_centers_in_meters,
+      const double radius_of_wheel_in_meters)
+      : length_between_wheel_centers_in_meters_(
+            length_between_wheel_centers_in_meters),
+        radius_of_wheel_in_meters_(radius_of_wheel_in_meters),
+        kinematics_matrix_{} {
+    const auto& r = radius_of_wheel_in_meters_;
+    const auto& L = length_between_wheel_centers_in_meters_;
+    kinematics_matrix_ << r * 0.5, r * 0.5, r / L, -r / L;
+    inverse_kinematics_matrix_ = kinematics_matrix_.inverse();
+  }
+
+  bool EvaluateStateDerivative(
+      const double* const current_state_vector,
+      const double* const current_input_vector,
+      double* current_state_derivative_vector) const final {
+    const double world_to_robot_position_x = current_state_vector[0];
+    const double world_to_robot_position_y = current_state_vector[1];
+    const double world_to_robot_position_psi = current_state_vector[2];
+    const double body_linear_velocity = current_input_vector[0];
+    const double body_angular_velocity = current_input_vector[1];
+
+    const double cos_angle = std::cos(world_to_robot_position_psi);
+    const double sin_angle = std::sin(world_to_robot_position_psi);
+
+    current_state_derivative_vector[0] = body_linear_velocity * cos_angle;
+    current_state_derivative_vector[1] = body_linear_velocity * sin_angle;
+    current_state_derivative_vector[2] = body_angular_velocity;
+
+    return true;
+  }
+
+ public:
+  const Eigen::Matrix<double, 2, 2>& GetKinematicsMatrix() const {
+    return kinematics_matrix_;
+  }
+
+  const Eigen::Matrix<double, 2, 2>& GetInverseKinematicsMatrix() const {
+    return inverse_kinematics_matrix_;
+  }
+
+ private:
+  double length_between_wheel_centers_in_meters_;
+  double radius_of_wheel_in_meters_;
+  Eigen::Matrix<double, 2, 2>
+      kinematics_matrix_;  // wheel angular velocities to body linear and
+                           // angular velocities
+  Eigen::Matrix<double, 2, 2> inverse_kinematics_matrix_;
+};
 
 int main() {
   int a = 0;
+
+  const double L = 0.65;  // [m]
+  const double r = 0.12;  // [m]
+  auto de = SizedStateDerivativeFunction<TwoWheeledRobotKinematics, 3, 2>(
+      new TwoWheeledRobotKinematics(L, r));
+
+  Eigen::Matrix<double, 3, 1> X_k;
+  Eigen::Matrix<double, 2, 1> u_k;
+  Eigen::Matrix<double, 3, 1> dX_k;
+  X_k << 4.2, -2.14, 0.91;
+  u_k << 1.0, 0.14;
+
+  de.EvaluateStateDerivative(X_k, u_k, &dX_k);
 
   return 0;
 }
